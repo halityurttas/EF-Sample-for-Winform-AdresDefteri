f3e60842270247591a4efe9c6eb8bdeef7bcf012
halityurttas <halityurttas@gmail.com>
2015-02-14 10:09:28 +0200

    Lesson of EF Sample For WinForms

---
+++ AdresDefteri/Form1.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace AdresDefteri
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            DAL.DatalarEntities context = new DAL.DatalarEntities();
+            var enuTelefonlar = from t in context.telefonlar
+                                select t;
+            this.dataGridView1.DataSource = enuTelefonlar.ToList();
+        }
+
+        private void btnAdd_Click(object sender, EventArgs e)
+        {
+            Form2 frm2 = new Form2();
+            frm2.ShowDialog();
+            Form1_Load(sender, e);
+        }
+    }
+}
---
+++ AdresDefteri/Form2.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace AdresDefteri
+{
+    public partial class Form2 : Form
+    {
+        public Form2()
+        {
+            InitializeComponent();
+        }
+
+        private void btnKaydet_Click(object sender, EventArgs e)
+        {
+            DAL.telefonlar tlf = new DAL.telefonlar();
+            tlf.AdiSoyadi = this.txtAdiSoyadi.Text;
+            tlf.Telefon = this.txtTelefon.Text;
+            tlf.GSM = this.txtGSM.Text;
+            DAL.DatalarEntities de = new DAL.DatalarEntities();
+            de.telefonlar.Add(tlf);
+            de.SaveChanges();
+            this.Close();
+        }
+    }
+}
